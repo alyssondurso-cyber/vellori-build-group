be5314a763f6436c7cdb05ae4c3311881a62d8af
alyssondurso-cyber <alyssondurso@gmail.com>
2026-08-15 15:07:11 -0400

    Update vite.config.js
    Add stucco page to Vite build

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         services: resolve(__dirname, "services.html"),
+        stuccoBocaRaton: resolve(__dirname, "stucco-boca-raton.html"),
       },
     },
   },
