a28e47e7b0d8dda56e8e0d8844fce7c9df80d96f
alyssondurso-cyber <alyssondurso@gmail.com>
2026-08-15 13:06:58 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import ServicesPage from "./ServicesPage.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const path = window.location.pathname;
+
+const Page =
+  path === "/services" || path === "/services/"
+    ? ServicesPage
+    : App;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Page />
+  </React.StrictMode>
+);
