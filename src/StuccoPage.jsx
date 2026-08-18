7f09191c06213e43a6a502236b21de134bfb223f
alyssondurso-cyber <alyssondurso@gmail.com>
2026-08-17 22:40:42 -0400

    Update StuccoPage.jsx

--- src/StuccoPage.jsx
+++ src/StuccoPage.jsx
@@ -22,13 +22,24 @@ const stuccoServices = [
   "Exterior Finish Repairs",
 ];
 
-const areas = [
+  const areas = [
   "Boca Raton",
   "Delray Beach",
+  "Highland Beach",
   "Palm Beach",
   "West Palm Beach",
+  "Palm Beach Gardens",
+  "Jupiter",
   "Parkland",
+  "Coral Springs",
+  "Pompano Beach",
   "Fort Lauderdale",
+  "Hollywood",
+  "Aventura",
+  "Sunny Isles Beach",
+  "Bal Harbour",
+  "Miami Beach",
+  "Miami",
 ];
 
 export default function StuccoPage() {
